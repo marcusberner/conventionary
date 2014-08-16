6535d88090ffc3a13525c6ca0008e5a3cee8ea68
Marcus Berner <marcus@burner.se>
2014-08-16 15:22:42 +0200

    Added dependency injection

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 module.exports = function(options, callback) {
 
-	var app = require('express')();
+	var app = require('express')(),
+		sandal = require('./lib/init/registerDependencies.js')();
 
-	require('./lib/init/loadWidgets.js')();
-	require('./lib/init/loadRoutes.js')(app);
-	require('./lib/init/registerLessRoute.js')(app, options);
-	require('./lib/init/registerScriptRoute.js')(app, options);
-	require('./lib/init/registerStaticRoute.js')(app, options);
-
-	callback(null, app);
-
+	require('./lib/init/loadWidgets.js')(sandal, function (err) {
+		if (err) return callback(err);
+		require('./lib/init/loadRoutes.js')(app, sandal, function (err) {
+			if (err) return callback(err);
+			require('./lib/init/registerLessRoute.js')(app, options);
+			require('./lib/init/registerScriptRoute.js')(app, options);
+			require('./lib/init/registerStaticRoute.js')(app, options);
+			callback(null, app);
+		});
+	});
 
 };
