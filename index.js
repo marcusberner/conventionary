bf7333af5cc971c94a0e35a2248c31d763a321b9
Marcus Berner <marcus@burner.se>
2014-08-20 13:49:43 +0200

    Support for injecting init functions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var async = require('async');
+
 module.exports = function(options, callback) {
 
 	if (!callback) {
@@ -5,24 +7,38 @@ module.exports = function(options, callback) {
 		options = {};
 	}
 
-	// Filters
-	require('./lib/filters/cacheBust.js');
-	require('./lib/filters/translate.js')(options.dictionaries || []);
-
 	require('./lib/init/setDefaults.js')(options);
 
-	var app = require('express')(),
-		sandal = require('./lib/init/registerDependencies.js')(options);
+	var express = require('express'),
+		app = express(),
+		sandal = require('./lib/init/registerDependencies.js')(app, options);
 
-	require('./lib/init/loadWidgets.js')(sandal, function (err) {
+	sandal.resolve(function (err, init) {
+
 		if (err) return callback(err);
-		require('./lib/init/loadRoutes.js')(app, sandal, function (err) {
+
+		async.parallel(init, function (err) {
+
 			if (err) return callback(err);
-			require('./lib/init/registerLessRoute.js')(app, options);
-			require('./lib/init/registerScriptRoute.js')(app, options);
-			require('./lib/init/registerStaticRoute.js')(app, options);
-			callback(null, app);
+			sandal.remove('init');
+
+			// Filters
+			require('./lib/filters/cacheBust.js');
+			require('./lib/filters/translate.js')(options.dictionaries || []);
+
+			require('./lib/init/loadWidgets.js')(sandal, function (err) {
+				if (err) return callback(err);
+				require('./lib/init/loadRoutes.js')(app, sandal, function (err) {
+					if (err) return callback(err);
+					require('./lib/init/registerLessRoute.js')(app, options);
+					require('./lib/init/registerScriptRoute.js')(app, options);
+					require('./lib/init/registerStaticRoute.js')(app, options);
+					callback(null, app);
+				});
+			});
+
 		});
+
 	});
 
 };
