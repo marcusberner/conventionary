876089ae710d34a6c300a29e1cd28578ef81912a
Marcus Berner <marcus@burner.se>
2014-09-19 08:51:25 +0200

    Define widget class once

--- init/03_registerLessRoute.js
+++ init/03_registerLessRoute.js
@@ -10,26 +10,26 @@ module.exports = function (app, options, lessCompiler, widgets, siteSandal) {
 		app.get(path.join(virtualStyleFolder, '*'), function (req, res, next) {
 			getFilePathAndType(req.path, function (pathAndType) {
 				if (!pathAndType) return next();
-				getLess(siteSandal, req, pathAndType, function (err, data) {
+				getLess(siteSandal, req, pathAndType, function (err, less) {
 					if (err) return next(err);
-					data = data || '';
+					less = less || '';
 					if (req.query.includeWidgets === 'true') {
 						widgets.forEach(function(widget) {
 							if (!widget.less) return;
-							if (widget.wrap) data += ('.widget-' + widget.path.join('_') + '{');
-							data += (' @import \''+ path.relative(path.dirname(pathAndType.path), widget.less) + '\'; ');
-							if (widget.wrap) data += '}';
+							if (widget.wrap) less += ('.' + widget.cssClass + '{');
+							less += (' @import \''+ path.relative(path.dirname(pathAndType.path), widget.less) + '\'; ');
+							if (widget.wrap) less += '}';
 						});
 					}
 					if (req.query.includeWidgetTheme) {
 						widgets.forEach(function(widget) {
 							if (!widget.themes[req.query.includeWidgetTheme]) return;
-							if (widget.wrap) data += ('.widget-' + widget.path.join('_') + '{');
-							data += (' @import \''+ path.relative(path.dirname(pathAndType.path), widget.themes[req.query.includeWidgetTheme].less) + '\'; ');
-							if (widget.wrap) data += '}';
+							if (widget.wrap) less += ('.' + widget.cssClass + '{');
+							less += (' @import \''+ path.relative(path.dirname(pathAndType.path), widget.themes[req.query.includeWidgetTheme].less) + '\'; ');
+							if (widget.wrap) less += '}';
 						});
 					}
-					lessCompiler(pathAndType.path, data, function (err, css) {
+					lessCompiler(pathAndType.path, less, function (err, css) {
 						if (err) return next(err);
 						if (!css) return next();
 						sendCss(res, css);
