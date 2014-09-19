8d16e4178473c291e249241c15c2da33011c03bf
Marcus Berner <marcus@burner.se>
2014-09-19 11:20:05 +0200

    Enabled AJAX loading of widgets

--- init/00_registerWidgetRoute.js
+++ init/00_registerWidgetRoute.js
@@ -1,17 +1,45 @@
 
-module.exports = function (app) {
+module.exports = function (app, widgets, renderTemplate, randomString, wrapWidget) {
 
 	return function(callback) {
 
-		app.post('/widgets/:type', function(req, res){
+		app.post('/widgets.*', function(req, res, next){
+
+			var widget = widgets.filter(function (widget) {
+				return widget.type === req.params['0'];
+			})[0];
+
+			if (!widget) return next();
 
 			var body = '';
 			req.on('data', function(data) {
 				body += data;
 			});
 			req.on('end', function() {
-				console.log('body', body);
-				res.send(req.params.type);
+				try {
+					var params = (body === '') ? {} : JSON.parse(body),
+						wrap = widget.wrap,
+						identifier = 'widget-' + randomString(),
+						that = {};
+					if (req.query.wrap === 'true') wrap = true;
+					else if (req.query.wrap === 'false') wrap = false;
+					widget.test.bind(that)(params, function (err, result) {
+						if (err) return next(err);
+						if (!result) return next();
+						widget.factory.bind(that)(params, function (err, widgetModel, options) {
+							if (err) return next(err);
+							options = options || {};
+							var templatePath = widget.template;
+							if (options.template) templatePath = path.join(widget.dir, options.template);
+							renderTemplate(templatePath, widgetModel, function (err, widgetHtml) {
+								if (err) return next(err);
+								res.send(wrap ? wrapWidget(widget, identifier, widgetHtml) : widgetHtml);
+							});
+						});
+					});
+				} catch (e) {
+					return next(e);
+				}
 			});
 		});
 
